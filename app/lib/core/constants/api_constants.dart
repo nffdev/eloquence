bbb02f6c80da94cd46979a8b53c76bac7b9acf5d
nffdev <nightfrost2discord@outlook.fr>
2025-07-15 15:38:24 +0200

    feat(app): updated api url

--- app/lib/core/constants/api_constants.dart
+++ app/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://127.0.0.1:8080'; // https://api.eloquence.social
+  static const String baseUrl = 'https://api.eloquence.social'; 
   static const String wordOfTheDayEndpoint = '/words/today'; // /words/today
   static const String wordOfTheDayUrl = '$baseUrl$wordOfTheDayEndpoint';
   
